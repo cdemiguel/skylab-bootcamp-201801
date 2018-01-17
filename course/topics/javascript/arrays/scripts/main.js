b9fc246e2b861e72f3ac32d8959fa16aca352726
cdemiguel <demiguelferrerocarlos@gmail.com>
2018-01-17 23:45:42 +0100

    Se hacen ejercicios hasta mergeArrays sobre arrays

--- course/topics/javascript/arrays/scripts/main.js
+++ course/topics/javascript/arrays/scripts/main.js
@@ -68,3 +68,62 @@ function generateArrayLength(a,b){
     console.log(arrayGen)
 }
 generateArrayLength(1,4)
+
+//last
+function last(a,b){
+    if(b){
+	for(var i = 0; i < b; i++){
+        console.log(a[i])
+    }}else{
+        console.log(a)   
+    }
+}
+console.log(last([7, 9, 0, -2],3));
+
+//sortItems
+var arr1 = [ 3, 8, 7, 6, 5, -4, 3, 2, 1 ]
+function sortItems(){
+    var arr2 = arr1.sort();
+    return arr2
+}
+console.log(sortItems())
+
+//getRandom
+var arrRandomItem = [2,3,4,6,8,9,"pedro"]
+function getRandom(){
+    arrRandomsItemNumber = arrRandomItem.length
+    var randomNumber = Math.floor(Math.random() * Math.floor(arrRandomsItemNumber));
+    console.log(arrRandomItem[randomNumber])
+}
+getRandom()
+
+//findDuplicates
+var arrDuplicates = [3,3,7,8,9,6,8]
+function findDuplicates(){
+    for(var i = 0; i < arrDuplicates.length; i++){
+        var number = arrDuplicates[i]
+        for(var j = 0; j < arrDuplicates.length; j++){
+            var number2 = arrDuplicates[j]
+            if(number === number2){
+                console.log(number + " value duplicate")
+            }else{
+                console.log(number + " value no duplicate")
+            }
+
+        }
+    }
+}
+findDuplicates()
+// TODO:
+
+//Merge arrays
+var array1 = [1, 2, 3]; 
+var array2 = [2, 30, 1]; 
+var arrayComplete = array1 +"," + array2
+arrayComplete = arrayComplete.split()
+var array = JSON.parse("[" + arrayComplete + "]");
+var arrayNoDuplicate = array.reduce(function(a,b){
+    if (a.indexOf(b) < 0 ) a.push(b);
+    return a;
+  },[]);
+console.log(arrayNoDuplicate)
