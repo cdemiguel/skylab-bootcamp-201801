12725ba6446cc5b23e09bba0e2aa23fd16ecf2de
cdemiguel <demiguelferrerocarlos@gmail.com>
2018-01-25 15:01:15 +0100

    últimos cambios en proyecto sass

--- course/frontend/higher_order_function/scripts.js/main.js
+++ course/frontend/higher_order_function/scripts.js/main.js
@@ -25,18 +25,132 @@ console.log(filterItems('j'));
 
 //Reduce 
 //Hacer un ejercicio 
-//Escribir una funcion que cuente todas las vocales del texto
-//reduce crea un contador
-
-
-
-//Hacer el ejercicio de reduce con regular expresion
+//Mi solucion:
+var text = 'the quick brown fox jumps over the lazy dog'
+text = text.split("")
+function countVowels(){
+    var vocals = 'aeiou'
+    var count = text.reduce(function(acc, valorActual){    
+    for(var i = 0; i < vocals.length; i++){
+        if(valorActual===vocals[i]){
+           return acc++;
+        }
+        return acc;
+    }
+  }, 0);
+  return count;
+}
+console.log(countVowels())
 
 //Every&some
 // con que uno cumple de true 
 //every si todas cumplen esa condicion false
 //some solo cuando una lo cumpla ya da respuesta true
 
+function checkMoreThanFiveLetters(months){
+    var result=months.every(function(x){
+        return x.length>=5;
+    });
+    return result;
+}
+checkMoreThanFiveLetters(['january','february','march','april','may','june','july','august', 'september','october','november','december'])
+// En cuanto encuentre ya uno que no cumpla la condicion cambiara el flag a false, porque encuentra uno ya que no la cumple
+//verifica si todos los elementos en el arreglo pasan la prueba implementada por la función dada.
+
+// en cambio el some
+//El método some() verifica si algún elemento de un array cumple con el test implementado por la función brindada.
+//por eso me dará true porque solo ya hay uno que cumplke la condicion ya hay un mes que es mayor que 5 entonces ya es true
+function checkMoreThanFiveLetters(months){
+    var result=months.some(function(x){
+        return x.length>=5;
+    });
+    return result;
+}
+checkMoreThanFiveLetters(['january','february','march','april','may','june','july','august', 'september','october','november','december'])
+
+
 //Callback
 // pasas una funcion a otra funcion como si fuese un parametro
 // Se separan responsabilidades... una funcion devolvera el permiso de mayor y mejor y otro el mensaje
+
+
+var permision = function allowOrNot(age){
+	var flag = function showMessage(){
+		if(age>18){
+            flag = true
+        }else{
+            flag = false
+        }
+    }
+    return flag;
+}
+function showMessage(permision){
+    if(flag==true){
+        console.log("puedes beber alcohol")
+    }else{
+        console.log("no puedes ENTRAR NI BEBER")
+    }}
+permision(8)
+
+
+
+
+function allowOrNot(age) {
+    var subject = "Esta permitda la entrada";
+    var subject2 = "NO se permite la entada"
+    var message = function () { 
+        if(age>18){
+            message = "Esta permitda la entrada"
+        }else{
+            message = subject2
+        }
+    };
+    return message;
+}
+var finalMessage = allowOrNot(9)
+console.log(finalMessage())
+//////////////////////////
+function allowOrNot(age) {
+    var message = function (age) { 
+        if(age>18){
+            message = "Esta permitda la entrada";
+        }else{
+            message = "NO se permite la entada";
+        }
+    return message;
+    };
+    return message;
+}
+var finalMessage = allowOrNot(9)
+console.log(finalMessage())
+///////////////////////////
+function allowOrNot(age) {
+    var message = function () { 
+        if(age>18){
+            message = "Esta permitda la entrada";
+        }else{
+            message = "NO se permite la entada";
+        }
+    	return message;
+    };
+    return message;
+}
+var finalMessage = allowOrNot(15)
+console.log(finalMessage())
+////////////////////////
+
+function defineMessage(){
+    var age=19;
+    var message = function () { 
+        if(age>18){
+        return "se permite la entrada"
+        }else{
+            return "no se permite la entrada"
+        }
+    };
+    return message;
+}
+ 
+var showMessage = defineMessage();
+console.log(showMessage())
+
